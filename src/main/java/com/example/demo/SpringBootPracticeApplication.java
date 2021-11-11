ba9fadd3d2d73b2552fbfd100eda448a6d0e5651
ava.chang <T3196@testritegroup.com>
2021-11-11 15:37:12 +0800

    rename project

--- src/main/java/com/example/demo/SpringBootPracticeApplication.java
+++ src/main/java/com/example/demo/SpringBootPracticeApplication.java
@@ -2,12 +2,11 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
-public class SpringBootPractiseApplication {
+public class SpringBootPracticeApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringBootPractiseApplication.class, args);
+		SpringApplication.run(SpringBootPracticeApplication.class, args);
 	}
 }
